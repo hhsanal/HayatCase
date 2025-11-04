2fc062619528fcdf748a08119fb68a0d3030f865
Hasan Sevinç <Hasan.Sevinc@hotmail.com.tr>
2025-11-04 10:44:32 +0300

    savechangesasync metodu genişletildi

--- Persistance/Context/AppDbContext.cs
+++ Persistance/Context/AppDbContext.cs
@@ -1,6 +1,7 @@
 ﻿using Domain.Abstractions;
 using Domain.Entities;
 using Microsoft.EntityFrameworkCore;
+using System.Security.Claims;
 
 namespace Persistance.Context;
 
@@ -8,6 +9,25 @@ public class AppDbContext : DbContext , IUnitOfWork
 {
     public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
     {
+    }
+    public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+    {
+        var entries = ChangeTracker.Entries<BaseEntity>();
+
+        foreach (var entry in entries)
+        {
+            if (entry.State == EntityState.Added)
+            {
+                entry.Property(x => x.CreatedDate).CurrentValue = DateTime.UtcNow;
+               
+            }
+
+            if (entry.State == EntityState.Deleted)
+            {
+                throw new ArgumentException("Db'den direkt silme işlemi yapamazsınız");
+            }
+        }
+        return base.SaveChangesAsync(cancellationToken);
     }
 
     public DbSet<FactorySensor> FactorySensors { get; set; }
